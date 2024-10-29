a32bff7c30dbd30388daf63d4dd5c96423b33a4c
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-29 12:27:58 +0000

    Completed task 2 - dataDimesions()

--- dfr.js
+++ dfr.js
@@ -23,15 +23,21 @@ function testInputs(funcName, arrayInputs, expectedOutputs) {
     let passed;
     try {
       output = funcName(input);
-      passed = output === expectedOutputs[index];
+      passed =
+        JSON.stringify(output) === JSON.stringify(expectedOutputs[index]);
     } catch (error) {
       output = `Error: ${error.message}`;
       passed = false;
     }
-    return { Input: input, Output: output, Passed: passed };
+    return {
+      Input: input,
+      Output: output,
+      Expected: expectedOutputs[index],
+      Passed: passed,
+    };
   });
 
-  console.table(results, ['Input', 'Output', 'Passed']);
+  console.table(results, ['Input', 'Output', 'Expected', 'Passed']);
 }
 
 // Validate whether value is a valid number or not
@@ -39,34 +45,48 @@ function validNumber(value) {
   return /^-?[0-9]+(\.[0-9]+)?$/.test(String(value));
 }
 
-// put iterator in for loop
+// Return number of rows and columns of a dataframe
 function dataDimensions(dataframe) {
   let rows = 0;
   let columns = 0;
-  for (let i = 0; i < dataframe.length; i++) {
-    rows += 1;
-    for (let j = 0; j < dataframe[i].length; j++) {
-      columns += 1;
+  try {
+    for (let i = 0; i < dataframe.length; i++) {
+      rows += 1;
+      for (let j = 0; j < dataframe[i].length; j++) {
+        if (i === 0) {
+          columns += 1;
+        }
+      }
     }
+    if (columns === 0) {
+      columns = -1;
+    }
+    if (rows === 0) {
+      rows = -1;
+    }
+    return [rows, columns];
+  } catch {
+    return [-1, -1];
   }
-  return [rows, columns];
 }
 
 // Testing
 
 const inputs = [
   [
-    [1000, 1100, 1200],
-    [1300, 1400, 1500],
-    [1600, 1700, 1800, 1900],
+    ['Q1', 1000, 1200, 950],
+    ['Q2', 1100, 1300, 975],
+    ['Q3', 1200, 1400, 1000],
   ], // salesData
   [1000, 1100], // Single-dimensional array
-  undefined, // No data
+  undefined,
+  '', // No data
 ];
 const expectedOutputs = [
   [3, 4], // Corresponding to salesData
   [2, -1], // Corresponding to a dataset with 2 elements
   [-1, -1], // Corresponding to no data
+  [-1, -1],
 ];
 
 // Assuming dataDimensions is your function to test
